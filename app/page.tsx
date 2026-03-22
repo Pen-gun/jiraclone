77554c27c415467a86ed472b0053448c77f4c043
Dragon <paraslimbu7115@gmail.com>
2026-03-22 14:22:20 +0545

    refactor: simplify button styles and layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
-    <Button variant='destructive' size='sm'>Click me</Button>
+    <Button>Click me</Button>
   )
 }
