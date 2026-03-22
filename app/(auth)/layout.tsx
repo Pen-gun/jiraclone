c63c9506796f09de8da31f6595070b656bf8a998
Dragon <paraslimbu7115@gmail.com>
2026-03-22 15:48:03 +0545

    feat: enhance Auth layout with navigation and logo, update SignIn page styling, and add logo.svg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 interface AuthLayoutProps {
     children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
     return (
-        <>
-        <h1>Auth Layout</h1>
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            {children}
-        </div>
-        </>
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    <Image src='/logo.svg' alt='logo' width={100} height={50} />
+                    <Button variant="secondary">Sign up</Button> 
+                </nav>
+                {children}
+            </div>
+        </main>
     )
 }
 export default AuthLayout;
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 export default function SignIn() {
     return (
-        <div>
+        <div className="flex items-center justify-center h-screen">
             <h1>sign-in</h1>
         </div>
     );
