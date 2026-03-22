c208d7859a8fb1952902206697b5a799405deb6f
Dragon <paraslimbu7115@gmail.com>
2026-03-22 16:20:06 +0545

    feat: implement DottedSeparator component and update SignInCard with new layout

--- features/auth/components/sign-in-card.tsx
+++ features/auth/components/sign-in-card.tsx
@@ -1,7 +1,21 @@
+import { DottedSeparator } from "@/components/dotted-seperator";
+import { 
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+ } from "@/components/ui/card";
 export const SignInCard = () => {
     return (
-        <div>
-            Sign In Card
-        </div>
+        <Card className="w-full h-full md:w-121.7 border-none shadow-none" >
+            <CardHeader className="flex items-center justify-center pt-10">
+                <CardTitle className="text-2xl font-bold text-center mb-4">
+                    Welcome my friend
+                </CardTitle>
+            </CardHeader>
+            <div className="px-7 mb-2">
+                <DottedSeparator />
+            </div>
+        </Card>
     );
 }
