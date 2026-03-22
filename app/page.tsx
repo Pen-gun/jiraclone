2747163a54391eb4624b03e244c22261c946139a
Dragon <paraslimbu7115@gmail.com>
2026-03-22 14:44:43 +0545

    feat: add new UI components including Drawer, DropdownMenu, Input, Label, Popover, ScrollArea, Select, Separator, Sheet, Skeleton, Sonner, Table, Tabs, and Textarea

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
+    <>
     <Button>Click me</Button>
+    <p className="text-2xl font-medium">Welcome to Next.js!</p>
+    </>
+    
   )
 }
