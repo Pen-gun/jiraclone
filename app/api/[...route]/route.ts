42b5fc6d73e428f4f70d3acca570eb7a44077877
Dragon <paraslimbu7115@gmail.com>
2026-03-24 10:55:07 +0545

    feat: add project route and query provider for React Query integration

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -7,5 +7,10 @@ app.get('/hello', (c) => {
     return c.json('Hello Node.js!')
 });
 
+app.get('/project/:projectId', (c)=>{
+    const {projectId} = c.req.param()
+    return c.json({projectId})
+})
+
 export const GET = handle(app)
 export const POST = handle(app)
