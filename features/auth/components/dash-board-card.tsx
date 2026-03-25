8ede5abc0945c9b3b759b32d49e617512cc27dea
Dragon <paraslimbu7115@gmail.com>
2026-03-25 16:01:11 +0545

    feat: enhance DashboardCard with improved layout and logout functionality

--- features/auth/components/dash-board-card.tsx
+++ features/auth/components/dash-board-card.tsx
@@ -1,26 +1,61 @@
 "use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
- } from "@/components/ui/dropdown-menu";
- import { DottedSeparator } from "@/components/dotted-seperator";
- import { useLogout } from "@/features/api/use-logout";
- import { useCurrent } from "@/features/api/use-current";
- import { Spinner } from "@/components/spinner";
+} from "@/components/ui/dropdown-menu";
+import { DottedSeparator } from "@/components/dotted-seperator";
+import { useLogout } from "@/features/api/use-logout";
+import { useCurrent } from "@/features/api/use-current";
+import { Spinner } from "@/components/spinner";
+import { LogOut } from "lucide-react";
 
 
- export const DashboardCard = () => {
+export const DashboardCard = () => {
 	const { data: user, isLoading } = useCurrent();
+	const { mutate: logout } = useLogout();
 	if (isLoading) {
 		return <Spinner />;
 	}
 	const { fullName, email } = user || {};
 	const avatarFallback = fullName ? fullName.charAt(0).toUpperCase() : "U";
 	return (
-		
+		<DropdownMenu modal={false}>
+			<DropdownMenuTrigger>
+				<Avatar className="size-10 hover:opacity-75 transition border border-neutral-50">
+					<AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+						{avatarFallback}
+					</AvatarFallback>
+				</Avatar>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end" side="bottom" className="w-60" sideOffset={10}>
+				<div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+					<Avatar className="size-13 border border-neutral-50">
+						<AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
+							{avatarFallback}
+						</AvatarFallback>
+					</Avatar>
+				</div>
+				<div className="flex flex-col items-center justify-center">
+					<p className="text-sm font-medium text-neutral-900">
+						{fullName || "Unknown User"}
+					</p>
+					<p className="text-xs text-muted-foreground">
+						{email || "No email provided"}
+					</p>
+				</div>
+				<DottedSeparator className="mb-1" />
+				<DropdownMenuItem className="h-10 flex items-center justify-center text-amber-700 font-medium cursor-pointer"
+					onClick={() => logout()}>
+					<LogOut className="size-4 mr-2" >
+						Log Out
+					</LogOut>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+
 	)
 
-}
+} 
